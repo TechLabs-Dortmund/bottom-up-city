eea2924812db08a29ef9fdd9e440f7caa75a40fd
Marioat2021 <85903896+Marioat2021@users.noreply.github.com>
2021-10-21 00:50:07 +0200

    New changes
    Search component now shows name and address of stations.
    Icons now shown on the map

--- React App with Leaflet/src/components/ui/MapExample.js
+++ React App with Leaflet/src/components/ui/MapExample.js
@@ -1,30 +1,39 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import Stations from "./stations.json";
+import React, { useState } from "react";
+import Stations from "../ui/stations.json"
 
-const MapExample = () => {      
+const Search = () => {
+    const [text, setText] = useState("");
     return (
-        <MapContainer center={[51.511028, 7.463520]} zoom={15} scrollWheelZoom={false}>
-            <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            
-            {Stations.filter((val) => 
-                        val.hasOwnProperty("lat") 
+        <section className="search">
+            <form>
+                <input type="text"
+                    className="form-control"
+                    placeholder="Search!"
+                    onChange={(e) => {
+                        setText(e.target.value);
+                    }}
+                    autoFocus
+                />    
+            </form>
+            <aside className="sidebar">
+                <div className="counter">
+                    <h4>Results</h4>
+                    <div className="filter">{Stations.filter((val) =>
+                        (text === "") ||
+                        (val.name.toLowerCase().includes(text.toLowerCase())) ||
+                        (val.address.toLowerCase().includes(text.toLowerCase()))
                         ).map((val, key) => {
-                        console.log(val);
-                            return (
-                                <Marker position={[val.lat, val.lng]}>
-                                    <Popup>
-                                        {val.name} <br /> {val.address}
-                                    </Popup>
-                                </Marker>
-                            );
-                        },{})}
-        </MapContainer>
+                        return (
+                            <div className="stelle" key={key}>
+                                <br /> {val.name} <br /> {val.address} <br />
+                            </div>
+                        );
+                    })}
+                    </div>
+                </div>
+            </aside>
+        </section>
     )
 }
 
-
-export default MapExample
+export default Search
